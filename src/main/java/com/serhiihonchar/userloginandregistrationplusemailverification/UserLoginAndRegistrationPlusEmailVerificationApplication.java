fc348133ef603c25ff55fac18dab0630883519c0
Serhii Honchar <sehonc@softserveinc.com>
2022-02-09 22:53:24 +0200

    add Repository, Service, Role, Entity

---
+++ src/main/java/com/serhiihonchar/userloginandregistrationplusemailverification/UserLoginAndRegistrationPlusEmailVerificationApplication.java
@@ -0,0 +1,13 @@
+package com.serhiihonchar.userloginandregistrationplusemailverification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginAndRegistrationPlusEmailVerificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserLoginAndRegistrationPlusEmailVerificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serhiihonchar/userloginandregistrationplusemailverification/UserLoginAndRegistrationPlusEmailVerificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serhiihonchar.userloginandregistrationplusemailverification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserLoginAndRegistrationPlusEmailVerificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

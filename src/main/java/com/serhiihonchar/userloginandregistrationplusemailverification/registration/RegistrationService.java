9e031bb2b3b4f080195d0a45cdafa57b091e70e7
Serhii Honchar <sehonc@softserveinc.com>
2022-02-12 22:51:42 +0200

    add Registration Service

--- src/main/java/com/serhiihonchar/userloginandregistrationplusemailverification/registration/RegistrationService.java
+++ src/main/java/com/serhiihonchar/userloginandregistrationplusemailverification/registration/RegistrationService.java
@@ -1,10 +1,32 @@
 package com.serhiihonchar.userloginandregistrationplusemailverification.registration;
 
+import com.serhiihonchar.userloginandregistrationplusemailverification.appuser.AppUser;
+import com.serhiihonchar.userloginandregistrationplusemailverification.appuser.AppUserRole;
+import com.serhiihonchar.userloginandregistrationplusemailverification.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
+
     public String register(RegistrationRequest request) {
-        return "it works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }

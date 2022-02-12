92242938d3299cb00d5f840ed347c76586dab6d3
Serhii Honchar <sehonc@softserveinc.com>
2022-02-12 21:38:31 +0200

    add refactored code

--- src/main/java/com/serhiihonchar/userloginandregistrationplusemailverification/security/PasswordEncoder.java
+++ src/main/java/com/serhiihonchar/userloginandregistrationplusemailverification/security/PasswordEncoder.java
@@ -1,5 +1,7 @@
 package com.serhiihonchar.userloginandregistrationplusemailverification.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
